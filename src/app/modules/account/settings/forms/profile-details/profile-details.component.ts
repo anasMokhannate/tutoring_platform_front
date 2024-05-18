4bc5bb29943a307888a2215d1ef6749749802729
aya <ayaelaajouri@gmail.com>
2024-05-18 15:56:28 +0100

    profile done

--- src/app/modules/account/settings/forms/profile-details/profile-details.component.ts
+++ src/app/modules/account/settings/forms/profile-details/profile-details.component.ts
@@ -4,12 +4,19 @@ import { BehaviorSubject, Subscription } from 'rxjs';
 @Component({
   selector: 'app-profile-details',
   templateUrl: './profile-details.component.html',
+  styleUrls: ['./profile-details.component.scss']
 })
 export class ProfileDetailsComponent implements OnInit {
   isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   isLoading: boolean;
   private unsubscribe: Subscription[] = [];
-
+  avatarUrl = './assets/media/avatars/blank.jpg';
+  firstName = 'Max';
+  lastName = 'Smith';
+  contactPhone = '044 3276 454 935';
+  email = 'max@kt.com';
+  role = 'student';
+  biography = '';
   constructor(private cdr: ChangeDetectorRef) {
     const loadingSubscr = this.isLoading$
       .asObservable()
